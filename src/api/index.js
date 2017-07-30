9f29b8a704c1016e50acbb19112121d4723577c5
qimuyunduan <84607842@qq.com>
2017-07-30 15:23:46 +0800

    use switchHost instead of webpack proxy

--- src/api/index.js
+++ src/api/index.js
@@ -10,6 +10,8 @@
  */
 import axios from "axios";
 
+const host = "http://www.pandatravel.com:4000",prefix = "/api";
+
 const Api = {
     /**
      * @description user interface for login
@@ -27,7 +29,72 @@ const Api = {
      */
     register(userName,password,code){
 
+    },
+    /**
+     * @description get user information
+     * @param userId
+     * @returns {AxiosPromise}
+     */
+    getInfo(userId) {
+        return axios.get(`${host}${prefix}/${userId}/info`);
+    },
+    /**
+     * @description get notifications  of userId
+     * @param userId
+     * @returns {AxiosPromise}
+     */
+    getNotifications(userId) {
+        return axios.get(`${host}${prefix}/${userId}/notifications`);
+    },
+    /**
+     * @description get notificationSetting of userId
+     * @param userId
+     * @returns {AxiosPromise}
+     */
+    getNotificationSetting(userId) {
+        return axios.get(`${host}${prefix}/${userId}/notificationSettings`);
+    },
+    /**
+     * @description get recommendations of userId
+     * @param userId
+     * @returns {AxiosPromise}
+     */
+    getRecommendations(userId) {
+        return axios.get(`${host}${prefix}/${userId}/recommendations`);
+    },
+    /**
+     * @description get personal info of userId
+     * @param userId
+     * @returns {AxiosPromise}
+     */
+    getPersonalInfo(userId) {
+        return axios.get(`${host}${prefix}/${userId}`);
+    },
+    /**
+     * @description get latest posts of userId
+     * @param userId
+     * @returns {AxiosPromise}
+     */
+    getPosts(userId){
+        return axios.get(`${host}${prefix}/${userId}/posts`);
+    },
+    /**
+     * @description get fans data of userId
+     * @param userId
+     * @returns {AxiosPromise}
+     */
+    getFans(userId){
+        return axios.get(`${host}${prefix}/${userId}/loves`);
+    },
+    /**
+     * @description get followers of userId
+     * @param userId
+     * @returns {AxiosPromise}
+     */
+    getFollowers(userId){
+        return axios.get(`${host}${prefix}/${userId}/followers`);
     }
+
 };
 
 export default Api;
